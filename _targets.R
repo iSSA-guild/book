8bbf58892ec1f5593ccbb8842a78f805ad5aac3c
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-07 14:34:52 -0700

    add targets:issa, book, all

--- _targets.R
+++ _targets.R
@@ -8,20 +8,18 @@
 # Source ------------------------------------------------------------------
 lapply(dir('R', '*.R', full.names = TRUE), source)
 
+# targets-issa
+raw_repo_path <- 'https://raw.githubusercontent.com/robitalec/targets-issa/'
+grab_functions(raw_repo_path)
 
 
 
-# Variables ---------------------------------------------------------------
-
-# TODO: license
-# TODO: contributing md
-# TODO: spelling
-# TODO: fix refs
-
-
+# Targets: targets-issa ---------------------------------------------------
 source('https://raw.githubusercontent.com/robitalec/targets-issa/main/_targets.R')
 
-c(
+
+# Targets: book -----------------------------------------------------------
+targets_book <- c(
 	tar_target(
 		generate_bib,
 		write_bib(c(.packages(), 'bookdown', 'knitr', 'rmarkdown'), 'packages.bib')
@@ -31,6 +29,12 @@ c(
 		{generate_bib; render_book('index.Rmd', output_dir = 'docs')}
 	)
 )
+
+
+# Targets: all ------------------------------------------------------------
+# Automatically grab and combine all the "targets_*" lists above
+lapply(grep('targets', ls(), value = TRUE), get)
+
 
 
 
