fb67201893bb9023b2cb95d54e844186aeabdfb4
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-07 14:35:13 -0700

    fix

--- R/grab_functions.R
+++ R/grab_functions.R
@@ -1,7 +1,7 @@
 grab_functions <- function(raw_repo_path) {
 	z <- gh("https://api.github.com/repos/robitalec/targets-issa/contents/R?recursive=1")
 
-	paths <- sapply(a, `[`, 'path')
+	paths <- sapply(z, `[`, 'path')
 
 	lapply(paste0(raw_repo_path, 'main/', grep('.R', paths, value = TRUE)),
 				 source)
