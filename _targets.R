75f5d3a09a680881fd4fcd2b5d3a973d89aa62b1
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-07 10:10:05 -0700

    force generate bib first

--- _targets.R
+++ _targets.R
@@ -14,7 +14,6 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 # Variables ---------------------------------------------------------------
 
 # TODO: [ALR] auto copy targets issa example to here
-# TODO: [ALR] https://docs.ropensci.org/targets/reference/tar_github_actions.html?q=action#ref-examples
 # TODO: license
 # TODO: contributing md
 # TODO: spelling
@@ -27,7 +26,7 @@ c(
 	),
 	tar_target(
 		book,
-		render_book('index.Rmd', output_dir = '_book')
+		{generate_bib; render_book('index.Rmd', output_dir = '_book')}
 	)
 )
 
