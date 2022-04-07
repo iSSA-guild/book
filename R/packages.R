c569c17babc645c32790b074fbcf6680776f04df
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-07 14:01:24 -0700

    add packages from targets-issa

--- R/packages.R
+++ R/packages.R
@@ -1,4 +1,7 @@
 library(targets)
+library(tarchetypes)
+library(qs)
+library(visNetwork)
 
 library(bookdown)
 library(knitr)
@@ -6,5 +9,16 @@ library(xml2)
 library(downlit)
 
 library(data.table)
+library(distanceto)
+library(parsedate)
 
 library(amt)
+library(sf)
+library(raster)
+
+library(renv)
+
+library(ggplot2)
+library(ggthemes)
+
+library(glmmTMB)
