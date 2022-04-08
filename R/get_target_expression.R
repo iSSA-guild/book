fe4cfdf8975eceda3aa7c4d1b5360fc9494da5ff
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-08 14:56:05 -0700

    fix error/stop

--- R/get_target_expression.R
+++ R/get_target_expression.R
@@ -7,7 +7,7 @@ get_target_expression <- function(target_list, name) {
 	which_name <- which(names == name)
 
 	if (length(which_name) != 1) {
-		error('name provided did not match to 1 target')
+		stop('name provided did not match to 1 target')
 	}
 
 	target_list[[which_name]]$command$expr[[1]]
