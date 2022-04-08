45cc5f9c8a0ed0ff0e711aed186660dfdcad8eeb
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-08 14:01:16 -0700

    add stop if not list

--- R/get_target_expression.R
+++ R/get_target_expression.R
@@ -1,4 +1,8 @@
 get_target_expression <- function(target_list, name) {
+	if (!is.list(target_list)) {
+		stop('target_list must be a list')
+	}
+
 	names <- vapply(target_list, function(x) x[['settings']][['name']], 'potato')
 	which_name <- which(names == name)
 
